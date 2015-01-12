94737a4c154f105d0c3c217373cc2af98e68994c
ounce <tdj.lwj@163.com>
2015-01-12 09:18:42 +0800

    将Explore中的TSwitch单元复制到本项目中，覆盖原单元。

--- TSwitch.cpp
+++ TSwitch.cpp
@@ -1,116 +1,564 @@
 //---------------------------------------------------------------------------
 
+
 #pragma hdrstop
 
 #include "TSwitch.h"
+#include "CoFunction.h"
+
+#include <math.h>
+
 //---------------------------------------------------------------------------
+
 #pragma package(smart_init)
 
-__fastcall SwitchAngleList::SwitchAngleList() {
-	SwitchAngle sa;
-    sa.Angle = 9.462222;
-    sa.FrogNumber = 6;
-    push_back(sa);
+extern TSwitchList SwitchList;
 
-    sa.Angle = 6.340278;
-    sa.FrogNumber = 9;
-    push_back(sa);
-
-    sa.Angle = 8.13;
-    sa.FrogNumber = 7;
-    push_back(sa);
-
-    sa.Angle = 8.079494;
-    sa.FrogNumber = 65;
-    push_back(sa);
+__fastcall TSwitch::TSwitch()
+{
+   	Type = Symmetry;
+	FH = 0;
+	SH = 24;
+	FPosition = 0;
+	FDirection = 0;
+	FBegin = 0;
+	FEnd = 0;
+	Fa = 0;
+	Fb = 0;
+	DrawSide = 0;
+	CircuitLength = 0;
+    FAngle = 0;
+    FFrogNumber = 0;
+    Init = false;
+	FRadii = 0;
 }
 
 //---------------------------------------------------------------------------
-void __fastcall TSwitch::AddLine(eLinePos Pos, TLine * Line) {
-    TSwitchLine sl;
-    sl.Pos = Pos;
-    sl.ptr = Line;
-    Lines.push_back(sl);
-    return;
+TSwitch & __fastcall TSwitch::operator = (const TSwitch & t)
+{
+	Fa = t.Fa;
+	Fb = t.Fb;
+	FAngle = t.FAngle;
+	FRadii = t.FRadii;
+	FLength = t.FLength;
+	CircuitLength = t.CircuitLength;
+	FModel = t.FModel;
+	FFrogNumber = t.FFrogNumber;
+	Angle = t.Angle;
+	FH = t.FH;
+	CH = t.CH;
+	SH = t.SH;
+	FPosition = t.FPosition;
+	ioPosition = t.ioPosition;
+	ioSortRailLength = t.ioSortRailLength;
+	SerialNumber = t.SerialNumber;
+	Name = t.Name;
+	FDirection = t.FDirection;
+	Type = t.Type;
+	Init = t.Init;
+	Number = t.Number;
+	FBegin = t.FBegin;
+	FEnd = t.FEnd;
+	ModelIndex = t.ModelIndex;
+    HandTypes = t.HandTypes;
+    UsedHandNumber = t.UsedHandNumber;
+    DrawSide = t.DrawSide;
+	return *this;
+};
+
+//---------------------------------------------------------------------------
+bool __fastcall TSwitch::Initialize(const TMeasurement Measurement)
+{
+   TSwitch Switch;
+
+	FPosition = ioPosition * Measurement.LengthRate;
+	SortRailLength = ioSortRailLength * Measurement.LengthRate;
+	Switch = SwitchList.Which(Model);
+	Fa = Switch.Fa;
+	Fb = Switch.Fb;
+	if (Switch.Type == DiamondCrossing)
+		SH = 0.5 * Switch.SH;
+	else
+		SH = (Direction == sdFORWARD) ? 0.5 * Switch.SH : Switch.SH;
+	FH = SH + CH;
+	Angle = Switch.Angle;    //单开对称道岔和三开道岔的角度不同，单开对称道岔的实际转角应为50%
+	Type = Switch.Type;
+//	FH = Switch.FH;
+	CircuitLength = Switch.CircuitLength;
+
+	FLength = Fa + Fb;
+	FGrade = FH / FLength;
+
+	if (Direction == sdBACKWARD) {
+		FBegin = Position - Fa;
+		FEnd = Position + Fb;
+	} else {
+		FBegin = Position - Fb;
+		FEnd = Position + Fa;
+	}
+
+	//设置导曲线半径
+   Init = true;
+   return Init;
+};
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitch::SetPosition(const float Value)
+{
+   FPosition = Value;
+   SetBeginEnd();
+};
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitch::SetBeginEnd(void)
+{
+	if (Direction)
+   {
+      FBegin = Position - Fa;
+      FEnd = Position + Fb;
+   }
+   else
+   {
+      FBegin = Position - Fb;
+      FEnd = Position + Fa;
+   }
+};
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitch::SetDirection(const int Value)
+{
+   FDirection = Value;
 }
 
 //---------------------------------------------------------------------------
-void __fastcall TSwitch::UpdateAngle(void) {
-	int i, j, m = -1;
-    double a = 90;
-    SwitchAngleList AngleList;
-    MainLineNumber = -1;
-	for (i = 0; i < Lines.size(); i++) {
-		if (InRange(X, Y, Lines[i].ptr->StartX, Lines[i].ptr->StartY, 1)) {
-            Lines[i].ptr->Angle = Lines[i].ptr->StartAngle;
-        } else if (InRange(X, Y, Lines[i].ptr->EndX, Lines[i].ptr->EndY, 1)) {
-        	Lines[i].ptr->Angle = OppositeAngle(Lines[i].ptr->StartAngle);
-        }
-        if (Lines[i].Pos == lpMIDDLE) {
-        	MainLineNumber = i;
-            Lines[i].Type = ltMAIN;
-        }
-    }
-    if (MainLineNumber > -1) {                      //如果有lpMIDDLE，设置其角度。
-	    j = (MainLineNumber - 1 < 0) ? MainLineNumber + 1 : MainLineNumber - 1;
-    	if (fabs(Lines[MainLineNumber].ptr->StartAngle - Lines[j].ptr->Angle) > 90) {
-       		Lines[MainLineNumber].ptr->Angle = Lines[MainLineNumber].ptr->StartAngle;
-	    } else {
-    	    Lines[MainLineNumber].ptr->Angle = OppositeAngle(Lines[MainLineNumber].ptr->StartAngle);
-	    }
-    }
-    MaxAngle = MinAngle = GetAngle(Lines[0].ptr->Angle, Lines[1].ptr->Angle);       //查找最大、最小角度
-    MaxAngleA = MaxAngleB = MinAngleA = MinAngleB = -1;
-    for (i = 0; i < Lines.size(); i++) {
-        for (j = i + 1; j < Lines.size(); j++) {
-        	a = GetAngle(Lines[i].ptr->Angle, Lines[j].ptr->Angle);
-            if (a > MaxAngle) {
-                MaxAngle = a;
-                MaxAngleA = i;
-                MaxAngleB = j;
-            }
-            if (a < MinAngle) {
-            	MinAngle = a;
-                MinAngleA = i;
-                MinAngleB = j;
-            }
-        }
-    }
-    for (i = 0; i < AngleList.size(); i++) {          //根据最小角查找对应的辙岔号。
-        if (EqualAngle(MinAngle, AngleList[i].Angle)) {
-        	FrogNumber = AngleList[i].FrogNumber;
+TSwitches & __fastcall TSwitches::operator = (const TSwitches & t)
+{
+   unsigned short i, q;
+
+   clear();
+   q = t.size();
+   for (i = 0; i < q; i++)
+      push_back(t[i]);
+
+	return *this;
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitch::SetHandNumber(unsigned short HandNumber) {
+    UsedHandNumber = HandNumber;
+    eSwitchHandType SwitchHandType = HandTypes[HandNumber];
+   	switch (SwitchHandType) {
+       	case shtSTRAIGHT:
+       		CH = 0;
             break;
-        }
+   	    case shtSIDE:
+       	    CH = Angle * 8;
+       		break;
+        case shtLEFT:
+   	    case shtRIGHT:
+			CH = 0.5 * Angle * 8;
+        	break;
+		default:
+   	    ;
     }
-    switch (Lines.size()) {
-    	case 2:
-        	if (MainLineNumber > -1) {          //单开道岔，直向和a值为一个线段。
-            	m = 1 - MainLineNumber;
-                switch (GetBranchSide(Lines[MainLineNumber].ptr->Angle, Lines[m].ptr->Angle)) {
-                    case bsLEFT:
-                    	//左侧单开道岔
-                }
-            }
-
-    default:
-        ;
-    }
+    DrawSide = 1 - HandNumber * 2;     //HandNumber == 0时，应为右，即 1； HandNumber == 1时，应为左，即 -1
     return;
 }
 
 //---------------------------------------------------------------------------
-void __fastcall TSwitch::Draw(TImage * Image, const double Scale, const double OriginX, const double OriginY) {
-	for (int i = 0; i < Lines.size(); i++) {
-		Lines[i].ptr->Draw(Image, Scale, OriginX, OriginY);
+__fastcall TSwitches::TSwitches()
+{
+//	PositionCaption.SetCaption(uLength, L"位置", "Position");
+//	SortRailLengthCaption.SetCaption(uLength, L"绝缘短轨长", "Sort Rail Length");
+}
+
+bool __fastcall TSwitches::VerifyInput(const WideString PositionText,
+									   const WideString SwitchNumberText,
+									   const WideString SortRailLengthText)
+{
+	float f;
+	TSwitchList List;
+	if (!IsFloat(PositionText, f, L"道岔位置不能为负数，也不能空白！", 0)) return false;
+	if (List.Index(SwitchNumberText) == 0) {
+		MessageBox(NULL, L"道岔型号必须是列表中的编号！", L"错误！", MB_OK);
+		return false;
+	}
+	if (!IsFloat(SortRailLengthText, f, L"绝缘短轨长度不能为负数，也不能空白！", 0)) return false;
+	return true;	
+}
+
+//---------------------------------------------------------------------------
+bool __fastcall TSwitches::Initialize(const TMeasurement Measurement)
+{
+   unsigned short i, q;
+
+   q = size();
+   for(i = 0; i < q; i++)
+		if (!at(i).Initialize(Measurement)) return false;
+
+   return true;
+};
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitches::Add(const WideString ModelName, const float Position)
+{
+   TSwitch Switch;
+
+   Switch.Model = ModelName;
+   Switch.Position = Position;
+
+   push_back(Switch);
+}
+
+//---------------------------------------------------------------------------
+__fastcall TSwitchList::TSwitchList()
+{
+	// Set H of all switches is 24 in New function.
+	// Other part of H will set in class TCurve.
+   TSwitch Switch;
+
+	Switch = New(sNone, L"Not Exist", 0, 0, 0, 0);
+	push_back(Switch);                  // ModelIndex 不能随意改动，会影响0版数据文件读取。
+
+	Switch = New(sSymmetry6, L"6#对称", 9.462222, 7.437, 9.994, 8.874, Symmetry);
+	Switch.CircuitLength = 7.82;
+	push_back(Switch);
+	Switch = New(sSimpleLeft9, L"9#左单开", 6.340278, 13.839, 15.009, 12.509, SimpleLeft);
+	Switch.CircuitLength = 15.455;
+	push_back(Switch);
+	Switch = New(sSimpleRight9, L"9#右单开", 6.340278, 13.839, 15.009, 12.509, SimpleRight);
+	Switch.CircuitLength = 15.455;
+	push_back(Switch);
+	Switch = New(sTreble7, L"7#三开", 8.1300000, 11.465, 12.658, 8.5, Treble);
+	Switch.CircuitLength = 7.748;
+	push_back(Switch);
+	Switch = New(sDiamondCrossing, L"菱形交叉", 0, 4.735, 4.735, 0, DiamondCrossing);
+	Switch.CircuitLength = 0;
+	push_back(Switch);
+	Switch = New(sSimpleLeft6, L"6#左单开", 9.462222, 8.491, 9.994, 8.874, SimpleLeft);
+	Switch.CircuitLength = 7.741;
+	push_back(Switch);
+	Switch = New(sSimpleRight6, L"6#右单开", 9.462222, 8.491, 9.994, 8.874, SimpleRight);
+	Switch.CircuitLength = 7.741;
+	push_back(Switch);
+    Switch = New(sSimpleLeft7, L"7#左单开", 8.130000, 10.897, 12.070, 10.839, SimpleLeft);
+	Switch.CircuitLength = 7.748;
+	push_back(Switch);
+    Switch = New(sSimpleRight7, L"7#右单开", 8.130000, 10.897, 12.070, 10.839, SimpleRight);
+	Switch.CircuitLength = 7.748;
+	push_back(Switch);
+	Switch = New(sSymmetry65, L"6.5#对称", 8.079494444, 8.913, 11.268, 8, Symmetry);
+	Switch.CircuitLength = 7.61;
+	push_back(Switch);
+	Switch = New(sSymmetry9, L"9#对称", 6.340278,13.839,15.009, 6.107, Symmetry);
+	Switch.CircuitLength = 15.455;
+	push_back(Switch);
+  	return;
+}
+
+//---------------------------------------------------------------------------
+TSwitch __fastcall TSwitchList::New(const unsigned short ModelIndex,
+									const WideString ModelName,
+									const float FrogNumber,
+									const float a,
+									const float b,
+									const float FH)
+{
+	TSwitch Switch;
+
+	Switch.ModelIndex = ModelIndex;
+	Switch.a = a;
+	Switch.b = b;
+	Switch.FrogNumber = FrogNumber;
+//   Switch.H = FH;
+//	Switch.H = 24;
+	Switch.Model = ModelName;
+
+	return Switch;
+};
+
+//---------------------------------------------------------------------------
+TSwitch __fastcall TSwitchList::New(const unsigned short ModelIndex,
+									const WideString ModelName,
+									const float Angle,
+									const float a,
+									const float b,
+									const float CircuitLength,
+									const eSwitchType Type)
+	{
+		TSwitch Switch;
+
+		Switch.ModelIndex = ModelIndex;
+		Switch.a = a;
+		Switch.b = b;
+		Switch.Angle = Angle;
+		Switch.Model = ModelName;
+		Switch.CircuitLength = CircuitLength;
+		Switch.Type = Type;
+//      0.139626337778 = 3.1415926 / 180 * 8
+		switch (Type) {
+			case Symmetry:
+				Switch.HandTypes.push_back(shtLEFT);
+                Switch.HandTypes.push_back(shtRIGHT);
+				break;
+			case SimpleLeft:
+				Switch.HandTypes.push_back(shtSIDE);
+                Switch.HandTypes.push_back(shtSTRAIGHT);
+				break;
+            case SimpleRight:
+				Switch.HandTypes.push_back(shtSTRAIGHT);
+                Switch.HandTypes.push_back(shtSIDE);
+				break;
+			case Treble:
+				Switch.HandTypes.push_back(shtLEFT);
+                Switch.HandTypes.push_back(shtSTRAIGHT);
+                Switch.HandTypes.push_back(shtRIGHT);
+				break;
+			case DiamondCrossing:
+				Switch.HandTypes.push_back(shtSTRAIGHT);
+                Switch.HandTypes.push_back(shtSTRAIGHT);
+				break;
+		default:
+			;
+		}
+
+		return Switch;
+	}
+
+
+//---------------------------------------------------------------------------
+TSwitch __fastcall TSwitchList::Which(const WideString ModelName)
+{
+	unsigned short i, q;
+
+	q = size();
+	for(i = 0; i < q; i++)
+		if (at(i).Model == ModelName)
+		{
+ //			List[i].H;      //?
+			return at(i);
+		}
+	return at(0);
+};
+
+//---------------------------------------------------------------------------
+TSwitch __fastcall TSwitchList::Which(const unsigned short ModelIndex)
+{
+	unsigned short i, q;
+	q = size();
+	for (i = 0; i < q; i++) {
+		if (at(i).ModelIndex == ModelIndex) {
+			return at(i);
+		}
+	}
+	return at(0);
+}
+
+//---------------------------------------------------------------------------
+unsigned short __fastcall TSwitchList::Index(const WideString ModelName)
+{
+   unsigned short i, q;
+
+   q = size();
+   for (i = 0; i < q; i++)
+      if (at(i).Model == ModelName)
+			return at(i).ModelIndex;
+	return at(0).ModelIndex;
+};
+
+//---------------------------------------------------------------------------
+WideString __fastcall TSwitchList::Model(const unsigned short ModelIndex)
+{
+	unsigned short i, q;
+	q = size();
+	for (i = 0; i < q; i++)
+	{
+		if (at(i).ModelIndex == ModelIndex)
+      {
+			return at(i).Model;
+		}
+	}
+	return at(0).Model;
+}
+                    /*
+TSwitch __fastcall TSwitchList::Modelconst unsigned short ModelIndex)
+{
+	unsigned short i, q;
+	q = size();
+	for (i = 0; i < q; i++)
+	{
+		if (at(i).ModelIndex == ModelIndex)
+      {
+			return at(i);
+		}
+	}
+	return at(0)
+}                                   */
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitch::Load(const int Handle)
+{
+	ioPosition = ReadFloat(Handle);
+	ioSortRailLength = ReadFloat(Handle);
+	ModelIndex = ReadInt(Handle);
+	Model = SwitchList.Model(ModelIndex);
+	Direction = ReadInt(Handle);
+//	ConnectWay = ReadInt(Handle);
+	DrawSide = ReadInt(Handle);
+	Type= ReadInt(Handle);
+//	if (DrawSide == -1) {
+//		DrawSide = 1;
+//	}
+
+}
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitch::Save(const int Handle)
+{
+	WriteFloat(Handle, ioPosition);
+	WriteFloat(Handle, ioSortRailLength);
+	WriteInt(Handle, ModelIndex);
+	WriteInt(Handle, Direction);
+//	WriteInt(Handle, ConnectWay);
+	WriteInt(Handle, DrawSide);
+	WriteInt(Handle, Type);
+}
+
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitches::Load(const int Handle)
+{
+	unsigned short i, q;
+	TSwitch Switch;
+	float p, l;
+	int Index;
+	WideString Model;
+
+	q = ReadFloat(Handle);
+//	resize(q);
+	clear();
+	for (i = 0; i < q; i++) {
+//      at(i).Load(Handle);
+		p = ReadFloat(Handle);
+		l = ReadFloat(Handle);
+		Index = ReadInt(Handle);
+		Model = SwitchList.Model(Index);
+		Switch = SwitchList.Which(Model);
+		Switch.ioPosition = p;
+		Switch.ioSortRailLength = l;
+		Switch.Direction = ReadInt(Handle);
+//		Switch.ConnectWay = ReadInt(Handle);
+		Switch.DrawSide = ReadInt(Handle);
+		Switch.Type= ReadInt(Handle);
+		push_back(Switch);
+	}
+	return;
+};
+
+
+//---------------------------------------------------------------------------
+void __fastcall TSwitches::Save(const int Handle)
+{
+   unsigned short i, q;
+
+   q = size();
+   WriteFloat(Handle, q);
+   for (i = 0; i < q; i++)
+		at(i).Save(Handle);
+};
+
+void __fastcall TSwitches::ResetIndex(void)
+{
+	for (int i = 0; i < size(); i++) at(i).Index = i;
+	return;
+}
+
+TSwitch __fastcall TSwitches::Find(const WideString SerialNumber)
+{
+	TSwitch A;
+	for (int i = 0; i < size(); i++)
+		if (at(i).SerialNumber == SerialNumber) return at(i);
+	A.ModelIndex = 0;
+	MessageBox(NULL, L"未找到相应的道岔数据，该数据可能已经被删除！", L"错误！", MB_OK + MB_ICONERROR);
+	return A;
+}
+
+ptrTSwitches __fastcall TSwitches::Find(const TSerialNumbers SerialNumbers)
+{
+    ptrTSwitches ptrSwitches;
+    for (int i = 0; i < size(); i++) {
+        for (int j = 0; j < SerialNumbers.size(); j++) {
+            if (SerialNumbers[j] == at(i).SerialNumber) {
+                ptrSwitches.push_back(&at(i));
+            }
+        }
+    }
+    return ptrSwitches;
+}
+
+
+void __fastcall TSwitches::WriteListView(TListView * ListView)
+{
+	int i;
+	TListItem * Item;
+//	TSwitchCharacterization Characterization;
+	ListView->Clear();
+	for (i = 0; i < size(); i++) {
+		Item = ListView->Items->Add();
+		Item->Caption = at(i).Name;
+		Item->SubItems->Add(FloatToStrF(at(i).ioPosition, 3));
+		Item->SubItems->Add(at(i).Model);
+		Item->SubItems->Add(FloatToStrF(at(i).ioSortRailLength, 3));
+//		Item->SubItems->Add(Characterization.StraightLateral[at(i).ConnectWay].ChineseCaption);
+		if (at(i).DrawSide < 0) {
+				at(i).DrawSide = 0;
+		}
+//		Item->SubItems->Add(Characterization.Direct[at(i).Direction].ChineseCaption);
+//		Item->SubItems->Add(Characterization.DrawDirect[at(i).DrawSide].ChineseCaption);
 	}
 	return;
 }
 
 //---------------------------------------------------------------------------
-void __fastcall TSwitches::Draw(TImage * Image, const double Scale, const double OriginX, const double OriginY) {
-	Image->Canvas->Pen->Color = clRed;
-	for (int i = 0; i < size(); i++) {
-		at(i).Draw(Image, Scale, OriginX, OriginY);
+float __fastcall TSwitch::GetInPos(void)
+{
+   return Position - a - SortRailLength;
+}
+
+//---------------------------------------------------------------------------
+float __fastcall TSwitch::GetOutPos(void)
+{
+	return Position - a + CircuitLength;
+}
+
+void __fastcall TSwitches::Draw(TCanvas *Canvas,
+								const int PlanBase,
+								const float Scale,
+								const float BeginPos,
+								const float EndPos,
+								const int PlanHeight)
+{
+	int i, q;
+	int x, y;
+
+	int Width;
+
+	Width = PlanHeight * 0.5 - 1;
+
+	q = size();
+	for (i = 0; i < q; i++)
+	{
+		x = Scale * (at(i).ioPosition - BeginPos);
+		y = PlanBase;
+		Canvas->MoveTo(x, y);
+		if (at(i).Direction > 0)
+			x = Scale * (at(i).ioPosition - BeginPos - at(i).b);
+		else
+			x = Scale * (at(i).ioPosition - BeginPos + at(i).b); //at(i).b);
+		if (at(i).DrawSide < 0)
+			y = PlanBase - Width; //DONE 1:加上方向判断
+		else
+			y = PlanBase + Width;
+      Canvas->LineTo(x, y);
 	}
-	return;
 }
--- TSwitch.h
+++ TSwitch.h
@@ -3,56 +3,162 @@
 #ifndef TSwitchH
 #define TSwitchH
 
-#include "Comm.h"
-#include "TLine.h"
+#include <system.hpp>
+#include <vector.h>
+#include <deque.h>
+#include <Graphics.hpp>
+#include "TSerialNumber.h"
+#include "TMeasurement.h"
 
 //---------------------------------------------------------------------------
-enum eLinePos {lpNONE, lpSTART, lpMIDDLE, lpEND };
-enum eLineType {ltUNKNOW, ltMAIN, ltBRANCH};
+
+//enum eConnectWay {Straight = 0, Side};
+enum eSwitchHandType {shtSTRAIGHT,	//直向
+                      shtSIDE,		//侧向
+                      shtLEFT,		//左
+                      shtRIGHT,		//右
+                      shtLEFTBACKWARD,	//左反向（菱形交叉使用）
+                      shtRIGHTBACKWARD};	//右反向
+enum eSwitchDirection {sdBACKWARD, sdFORWARD};     //逆向、 顺向
 enum eSwitchType {Symmetry = 0,	//单开对称道岔
                   SimpleLeft,				//左开单开道岔
                   SimpleRight,				//右开单开道岔
 				  Treble,			//三开对称道岔
                   DiamondCrossing};		//菱形交叉
+enum eSwitch {sNone, sSimpleLeft6, sSimpleLeft7, sSimpleLeft9, sSymmetry6, sSymmetry65, sSymmetry9, sTreble7, sDiamondCrossing,
+              sSimpleRight6, sSimpleRight7, sSimpleRight9};
 
-//---------------------------------------------------------------------------
-class SwitchAngle {
+#define TSwitchHandNumber unsigned short int
+
+class TSwitchHandNumbers : public vector<TSwitchHandNumber> {
+
+};
+
+class TSwitch : public TElementSerialNumber
+{
+private:
+	float Fa, Fb;
+	float FLength;
+	float FAngle;
+	float FRadii;
+
+	float FBegin, FEnd;
+	float FPosition;
+	int FDirection;
+	WideString FModel;
+	float FFrogNumber;
+	float FH;
+	float FGrade;
+
+	bool Init;
+
+    float __fastcall GetInPos(void);
+    float __fastcall GetOutPos(void);
+
+    void __fastcall SetPosition(const float Value);
+    void __fastcall SetBeginEnd(void);
+	void __fastcall SetDirection(const int Value);
+
 public:
-	double Angle;
-    int FrogNumber;
+	unsigned short Number;  //编号
+	eSwitch ModelIndex;
+	eSwitchType Type;
+	float CircuitLength;        //岔内轨道电路长度
+	float SortRailLength;       //绝缘短轨长度
+//	int DirectionQuantity;
+	float ioPosition, ioSortRailLength;
+	float Angle;
+
+    unsigned short UsedHandNumber;
+    vector<eSwitchHandType> HandTypes;		//按照从左至右存储个分支的性质，其数量就是连接的方向数。
+	int DrawSide;		// 绘图方向 1——左；	-1——右。 为了兼容0版数据文件格式保留这个变量。
+//	eHandType ConnectWay;    //  侧向
+//	bool Symmetry;		//是否为对称道岔。
+//	bool DiamondCrossing;		//是否是菱形交叉渡线。
+
+	float CH;  	//Curve Height	曲线能高
+	float SH;	// Switch Height	道岔能高
+
+	__property float a = { read=Fa, write=Fa };
+	__property float b = { read=Fb, write=Fb };
+	__property float Length = { read=FLength, write=FLength };
+	__property float Radii = { read=FRadii, write=FRadii };
+	__property float FrogNumber = { read=FFrogNumber, write=FFrogNumber };
+
+	__property float Position = { read=FPosition, write=SetPosition };
+	__property float InPos = { read=GetInPos };
+	__property float OutPos = { read=GetOutPos };
+	__property int Direction = { read=FDirection, write=SetDirection };	// 方向 1——顺向；0——逆向；
+	__property float Begin = { read=FBegin };
+	__property float End = { read=FEnd };
+
+   __property WideString Model = { read=FModel, write=FModel };
+
+   __property float H = { read=FH, write=FH };
+   __property float Grade = { read=FGrade, write=FGrade };
+
+   __fastcall TSwitch();
+	bool __fastcall Initialize(const TMeasurement Measurement);
+	TSwitch & __fastcall operator = (const TSwitch & t);
+	void __fastcall Save(const int Handle);
+	void __fastcall Load(const int Handle);
+    void __fastcall SetHandNumber(unsigned short HandNumber);
+};
+
+class ptrTSwitches : public vector<TSwitch *>
+{
+
+};
+
+class TSwitches : public vector<TSwitch>
+{
+public:
+	__fastcall TSwitches();
+//	TItemCaption PositionCaption, SortRailLengthCaption;
+	TSwitches & __fastcall operator = (const TSwitches & t);
+	bool __fastcall VerifyInput(const WideString PositionText,
+										 const WideString SwitchNumberText,
+										 const WideString SortRailLengthText);
+	bool __fastcall Initialize(const TMeasurement Measurement);
+	void __fastcall Add(const WideString ModelName, const float Position);
+	void __fastcall Save(const int Handle);
+	void __fastcall Load(const int Handle);
+	void __fastcall ResetIndex(void);
+	TSwitch __fastcall Find(const WideString SerialNumber);
+    ptrTSwitches __fastcall Find(const TSerialNumbers SerialNumbers);
+	void __fastcall WriteListView(TListView * ListView);
+
+	void __fastcall Draw(TCanvas *Canvas, const int PlanBase, const float Scale,
+								const float BeginPos, const float EndPos, const int PlanHeight);
 };
 
 //---------------------------------------------------------------------------
-class SwitchAngleList : public vector<SwitchAngle> {
+class TSwitchList : public vector<TSwitch>
+{
 public:
-	__fastcall SwitchAngleList();
+   __fastcall TSwitchList();
+//   TSwitchList & __fastcall operator = (const TSwitchList & List);
+	TSwitch __fastcall Which(const WideString ModelName);
+	TSwitch __fastcall Which(const unsigned short ModelIndex);
+	unsigned short __fastcall Index(const WideString ModelName);
+	WideString __fastcall Model(const unsigned short Index);
+//    TSwitch __fastcall Model(const unsigned short Index);
+private:
+	TSwitch __fastcall New(const unsigned short ModelIndex, const WideString ModelName,
+								  const float FrogNumber, const float a, const float b,
+								  const float FH);
+
+	TSwitch __fastcall New(const unsigned short ModelIndex, const WideString ModelName,
+								  const float Angle, const float a, const float b,
+								  const float CircuitLength,
+								  const eSwitchType Type);
 };
 
-//---------------------------------------------------------------------------
-class TSwitchLine {
-public:
-	eLinePos Pos;
-    eLineType Type;
-    TLine * ptr;
+class TSwitchesList : public vector<TSwitches>
+{
+
 };
 
-//---------------------------------------------------------------------------
-class TSwitch {
-public:
-	double X, Y;
-    double MinAngle, MaxAngle;
-    int MinAngleA, MinAngleB, MaxAngleA, MaxAngleB;
-    int FrogNumber, MainLineNumber;
-    eSwitchType Type;
-    vector<TSwitchLine> Lines;
-	void __fastcall AddLine(eLinePos Pos, TLine * Line);
-    void __fastcall UpdateAngle(void);
-	void __fastcall Draw(TImage * Image, const double Scale, const double OriginX, const double OriginY);
-};
-
-//---------------------------------------------------------------------------
-class TSwitches : public vector<TSwitch> {
-public:
-	void __fastcall Draw(TImage * Image, const double Scale, const double OriginX, const double OriginY);
-};
 #endif
+
+
